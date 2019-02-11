47e31fa801f438bfe2740df83b817805b14f9eb0
Guadalupe Vazquez <guvadlt@gmail.com>
2019-02-10 19:33:47 -0600

    Se agrego la ventana principal

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,9 +1,37 @@
 
-public class Prueba {
+import java.awt.Font;
+import java.awt.event.ActionListener;
 
+import javax.swing.*;
+
+class Calculadora extends JFrame{
+	
+	public Calculadora() {
+		setTitle("Calculadora chafa");
+		getContentPane().setLayout(null);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(300,300);
+		setVisible(true);
+		
+		
+		//componentes GUI
+		JLabel estandar = new JLabel("ESTÁNDAR");
+		estandar.setBounds(1, 0, 100, 10);
+		add(estandar);
+	}
+}
+
+public class Prueba {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new Calculadora();
+				
+			}
+		});
 
 	}
-
 }
